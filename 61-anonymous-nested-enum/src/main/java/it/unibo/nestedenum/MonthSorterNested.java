9b3cd4bcc59a8ba5e4768b344f06803db6c68087
Alin Bordeianu <alinstefan.bordeianu@studio.unibo.it>
2022-11-14 11:35:50 +0100

    Ho completato l'enum dei mesi, contiene costruttore privato e getters

--- 61-anonymous-nested-enum/src/main/java/it/unibo/nestedenum/MonthSorterNested.java
+++ 61-anonymous-nested-enum/src/main/java/it/unibo/nestedenum/MonthSorterNested.java
@@ -20,6 +20,39 @@ public final class MonthSorterNested implements MonthSorter {
     }
 
     public static enum Month {
-        JANUARY, FEBRUARY, MARCH, APRIL, MAY, JUNE, JULY, AUGUST, SEPTEMBER, OCTOBER, NOVEMBER, DECEMBER;
+        JANUARY("January", 31, 1),
+        FEBRUARY("February", 28, 2), 
+        MARCH("March", 31, 3), 
+        APRIL("April", 30, 4), 
+        MAY("May", 31, 5), 
+        JUNE("June", 30, 6),
+        JULY("July", 31, 7), 
+        AUGUST("August", 31, 8), 
+        SEPTEMBER("September", 30, 9),
+        OCTOBER("October", 31, 10),
+        NOVEMBER("November", 30, 11), 
+        DECEMBER("December", 31, 12);
+        
+        private final String actualName;
+        private final int numberOfDays;
+        private final int order;
+
+        private Month(String actualName, int numberOfDays, int order) {
+            this.actualName = actualName;
+            this.numberOfDays = numberOfDays;
+            this.order = order;
+        }
+
+        public String getActualName() {
+            return this.actualName;
+        }
+
+        public int getNumberOfDays() {
+            return this.numberOfDays;
+        }
+
+        public int getOrder() {
+            return this.order;
+        }        
     }
 }
