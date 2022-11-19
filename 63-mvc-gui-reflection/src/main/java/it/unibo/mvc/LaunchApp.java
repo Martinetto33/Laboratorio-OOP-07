bb099c27f1db52b22cc881547571861e042f669b
Alin Bordeianu <alinstefan.bordeianu@studio.unibo.it>
2022-11-19 12:19:47 +0100

    Ho iniziato la reflection modificando la classe LaunchApp; ci sono moltissimi try-catch da fare e non so se stia sbagliando qualcosa

--- 63-mvc-gui-reflection/src/main/java/it/unibo/mvc/LaunchApp.java
+++ 63-mvc-gui-reflection/src/main/java/it/unibo/mvc/LaunchApp.java
@@ -5,6 +5,9 @@ import it.unibo.mvc.controller.DrawNumberControllerImpl;
 import it.unibo.mvc.model.DrawNumberImpl;
 import it.unibo.mvc.view.DrawNumberSwingView;
 import it.unibo.mvc.view.StdoutDrawNumberView;
+import java.lang.Class;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
 
 /**
  * Application entry-point.
@@ -25,10 +28,35 @@ public final class LaunchApp {
      * @throws IllegalArgumentException in case of reflection issues
      */
     public static void main(final String... args) {
-        final var model = new DrawNumberImpl();
-        final DrawNumberController app = new DrawNumberControllerImpl(model);
-        app.addView(new StdoutDrawNumberView());
-        app.addView(new DrawNumberSwingView());
-        app.addView(new DrawNumberSwingView());
+        // final var model = new DrawNumberImpl();
+        // final DrawNumberController app = new DrawNumberControllerImpl(model);
+        // app.addView(new StdoutDrawNumberView());
+        // app.addView(new DrawNumberSwingView());
+        // app.addView(new DrawNumberSwingView());
+        /*Reflection Loading */
+        Class <?> model;
+        Class <?> app;
+        Class <?> view1;
+        Class <?> view2;
+        Class <?> view3;
+        try {
+            model = Class.forName("DrawNumberImpl"); //usare Class<?> perché Class è un raw type
+            Constructor<?> modelConstructor = model.getDeclaredConstructor(model.getClass());
+            app = Class.forName("DrawNumberControllerImpl");
+            view1 = Class.forName("StdoutDrawNumberView");
+            view2 = Class.forName("DrawNumberSwingView");
+            view3 = Class.forName("DrawNumberSwingView");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            System.exit(3);
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+            System.out.println("No \"getDeclaredConstructor\" method was found!");
+            System.exit(4);
+        } catch (SecurityException e) {
+            e.printStackTrace();
+            System.out.println("Security exception!");
+            System.exit(5);
+        }
     }
 }
